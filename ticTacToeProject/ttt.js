10273e391eead2fd47f474cb471e57bd2f6b6de4
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-01 16:33:20 -0500

    added functionality to ttt.js to change grid with click

--- ticTacToeProject/ttt.js
+++ ticTacToeProject/ttt.js
@@ -13,18 +13,30 @@ function blockOfBoard(index){
 }
 createBoard();
 
-// const Player = (name, marker)=>{
-//     return {name, marker};
-// };
+const Player = (name, marker) => {
+    return { name, marker };
+};
 
-// const playerOne=Player(player1, "X");
-// const playerTwo=Player(player2, "O");
+const playerOne=Player("player1", "X");
+const playerTwo=Player("player2", "O");
+
+let currPlay = playerOne;
+
 
 const gameBoard=(()=>{
     const boardArray = ["", "", "", "", "", "O", "", "X", ""];
     const setBoard = () => {
         for (let i=0; i<9; i++){
             document.getElementById(`square${i+1}`).innerHTML= boardArray[i];
+            document.getElementById(`square${i+1}`).onclick = function(){
+                boardArray[i]= currPlay.marker;
+                if(currPlay===playerOne){
+                    currPlay=playerTwo;
+                }else{
+                    currPlay=playerOne;
+                }
+                updateBoard();
+            };
         }
     };
     return {
@@ -33,4 +45,28 @@ const gameBoard=(()=>{
     };
 })();
 
-gameBoard.setBoard();
+
+const displayController = (()=>{
+    const currentPlayer = (player) => player.name;
+    const currentMarker = (player) => player.marker;
+    return {currentMarker,
+    currentPlayer};
+})();
+
+
+
+function getGameStatus(player){
+    document.getElementById("currentPlayer").innerHTML = `<b>Current Player:</b> ${displayController.currentPlayer(player)}`;
+    document.getElementById("currentMarker").innerHTML = `<b>Player's Marker:</b> ${displayController.currentMarker(player)}`;
+
+};
+
+
+
+
+function updateBoard(){
+    getGameStatus(currPlay);
+    gameBoard.setBoard();
+};
+updateBoard();
+
