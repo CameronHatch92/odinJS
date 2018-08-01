5dfeb9cfe91ec72bbf3ef19cc3476fb96af7da0e
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-01 09:42:04 -0500

    added form and buttons to library

--- libraryProject.js
+++ libraryProject.js
@@ -6,16 +6,28 @@ function Book(title, author, pages, read){
     this.pages = pages
     this.read = read
     this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    }
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`}
 }
 
 function addBookToLibrary(newBook){
     myLibrary.push(newBook);
 }
-
+let HP = new Book("Harry Potter", "J.K. Rowling", 400, "I have read this book");
+let Bible = new Book("Bible", "God", 2000, "I have read this book");
+myLibrary.push(HP);
+myLibrary.push(Bible);
 function render(bookArray){
+    let bookHTML = "";
     for (i=0; i<bookArray.length; i++){
-        
+        bookHTML += `<div class="card"><p> ${myLibrary[i].title}</p>
+        <p> ${myLibrary[i].author}</p>
+        <p> ${myLibrary[i].pages} pages </p>
+        <p> ${myLibrary[i].read}</p></div>`
     }
+    document.getElementById("bookContainer").innerHTML = bookHTML;
 }
+function addForm(){
+    document.getElementById("addBook").style.display="block";
+}
+
+render(myLibrary);
